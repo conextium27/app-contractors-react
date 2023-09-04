3a2d58a2b342699ff9330dad2f1483e22b115fca
rick <conextium@gmail.com>
2023-09-04 10:50:43 -0600

    chore: add files like components

---
+++ src/app/graaphql.tsx
@@ -0,0 +1,28 @@
+// import { gql, useApolloClient } from "@apollo/client";
+// import client from "../apollo-client";
+
+// export async function GetDataContractors() {
+
+//   const { data: { contractors } }: any = await client.query({
+//     query: gql`
+//       {
+//         contractors{
+//           data{
+//             attributes{
+//               fullname
+//               specialities
+//               dayrate
+//               availability
+//             }
+//           }
+//         }
+//       }
+//           `,
+//   });
+//   return contractors.data.map((value: any) => {
+
+//     const res = { ...value.attributes };
+
+//     return res;
+//   });
+// }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,21 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      colors: {
+        color_gray: 'rgba(51, 51, 51, 0.60)',
+        backgroud_gray: 'rgba(255, 255, 255, 0.60)',
+        backgrondgrandient:  'linear-gradient(119deg, #FCFFFD 13.94%, #F2F1FF 96.95%), var(--Pure-white, #FFF)',
+      },
+      boxShadow:{
+        boxshadow: '0px 49px 100px 0px rgba(0, 0, 0, 0.10)',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
