81d90ecd8930a21a1a8e6ee5a4a4c641fc8a4a54
rick <conextium@gmail.com>
2023-09-05 10:13:13 -0600

    change links

--- app/graaphql.tsx
+++ app/graaphql.tsx
@@ -1,37 +1,37 @@
-// const API_URL = 'http://localhost:1377/graphql';
-const API_URL =  'https://best-birds-5c1a4927fc.strapiapp.com/graphql';
+// const API_URL = 'http://127.0.0.1:1337/graphql';
 
-const CONTRACTORS_QUERY = `
-query{
-    contractors{
-      data{
-        attributes{
-          fullname,
-          specialities,
-          dayrate,
-          availability
-        }
-      }
-    }
-  }
-  `;
+// const CONTRACTORS_QUERY = `
+// query{
+//     contractors{
+//       data{
+//         attributes{
+//           fullname,
+//           specialities,
+//           dayrate,
+//           availability
+//         }
+//       }
+//     }
+//   }
+//   `;
 
-async function fetchData() {
-  const response = await fetch(API_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ query:CONTRACTORS_QUERY }),
-  });
+// async function fetchData() {
+//   const response = await fetch(API_URL, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({ query:CONTRACTORS_QUERY }),
+//   });
 
-  if (!response.ok) {
-    throw new Error('Error al realizar la consulta GraphQL');
-  }
-
-  const data = await response.json();
-  return data.data.contractors.data.map((value:any) => {
-    return value.attributes
-  });
-}
-export default fetchData;
+//   if (!response.ok) {
+//     throw new Error('Error al realizar la consulta GraphQL');
+//   }
+  
+//   const data = await response.json();
+//   return data.data.contractors.data.map((value:any) => {
+    
+//     return value.attributes
+//   });
+// }
+// export default fetchData;
